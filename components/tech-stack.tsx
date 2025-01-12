d98b75751b4556c1781449db6440829ab173ddcf
Bharat <btharwani92@gmail.com>
2025-01-12 15:33:33 -0500

    Added Logos

--- components/tech-stack.tsx
+++ components/tech-stack.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Button } from './ui/moving-borders'
-import { FaCss3, FaCss3Alt, FaDatabase, FaFacebook, FaGitSquare, FaGithub, FaHtml5, FaJava, FaJs, FaJsfiddle, FaLaravel, FaPhp, FaPython, FaReact, FaVuejs } from 'react-icons/fa'
+import { FaCss3, FaCss3Alt, FaDatabase, FaFacebook, FaFire, FaGitSquare, FaGithub, FaHtml5, FaJava, FaJs, FaJsfiddle, FaLaravel, FaNodeJs, FaPhp, FaPython, FaReact, FaVuejs } from 'react-icons/fa'
 import { companies } from "@/data";
 import { FaN, FaT } from 'react-icons/fa6';
 
@@ -13,66 +13,58 @@ const TechStack = () => {
         <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10">
         <Button
             duration={Math.floor(Math.random() * 10000) + 10000}
-            borderRadius="1.75rem"
-            // style={{
-            //   //   add these two
-            //   //   you can generate the color from here https://cssgradient.io/
-            //   background: "rgb(4,7,29)",
-            //   backgroundColor:
-            //     "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
-            //   // add this border radius to make it more rounded so that the moving border is more realistic
-            //   borderRadius: `calc(1.75rem* 0.96)`,
-            // }}
-            
-            className="text-white border-slate-800 bg-gradient-to-r from-slate-850 to-slate-950"
-          >
+            borderRadius="1.75rem" className="text-white border-slate-800 bg-gradient-to-r from-slate-850 to-slate-950"
+        >
                 
                 <div className='flex flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-10'>
                     <div className='text-5xl'>Frontend</div>
                     <div className="flex flex-rows gap-3 md:gap-3 lg:gap-8 flex-wrap items-center justify-center">
                         <div className="w-36 flex flex-col gap-2 py-3 px-6 text-xs lg:text-base opacity-50 
                             lg:opacity-100 rounded-lg justify-center items-center text-center bg-[#10132E] hover:scale-125 transform duration-1000">
-                            <FaHtml5 className='text-3xl text-blue-800 text-center' />
+                            <FaHtml5 className='text-3xl text-[#DD4A25] text-center' />
                             <span>HTML</span>
                         </div>
                         <div className="w-36 flex flex-col gap-2 py-3 px-6 text-xs lg:text-base opacity-50 
                             lg:opacity-100 rounded-lg justify-center items-center text-center bg-[#10132E] hover:scale-125 transform duration-1000">
-                            <FaCss3 className='text-3xl text-blue-800 text-center' />
+                            <FaCss3 className='text-3xl text-[#244BDE] text-center' />
                             <span>CSS</span>
                         </div>
                         <div className="w-36 flex flex-col gap-2 py-3 px-6 text-xs lg:text-base opacity-50 
                             lg:opacity-100 rounded-lg justify-center items-center text-center bg-[#10132E] hover:scale-125 transform duration-1000">
-                            <FaT className='text-3xl text-blue-800 text-center' />
+                            {/* <FaT className='text-3xl text-[#37BCF8] text-center' /> */}
+                            <img src='/tailwind.svg' className='w-8' />
                             <span>Tailwind CSS</span>
                         </div>
                         <div className="w-36 flex flex-col gap-2 py-3 px-6 text-xs lg:text-base opacity-50 
                             lg:opacity-100 rounded-lg justify-center items-center text-center bg-[#10132E] hover:scale-125 transform duration-1000">
-                            <FaJs className='text-3xl text-blue-800 text-center' />
+                            <FaJs className='text-3xl text-[#EFD81A] text-center' />
                             <span>JavaScript</span>
                         </div>
                         <div className="w-36 flex flex-col gap-2 py-3 px-6 text-xs lg:text-base opacity-50 
                             lg:opacity-100 rounded-lg justify-center items-center text-center bg-[#10132E] hover:scale-125 transform duration-1000">
-                            <FaJsfiddle className='text-3xl text-blue-800 text-center' />
+                            {/* <FaJsfiddle className='text-3xl text-[#2F73BF] text-center' /> */}
+                            <img src='/typescript.svg' className='w-8' />
                             <span>TypeScript</span>
                         </div>
                         <div className="w-36 flex flex-col gap-2 py-3 px-6 text-xs lg:text-base opacity-50 
                             lg:opacity-100 rounded-lg justify-center items-center text-center bg-[#10132E] hover:scale-125 transform duration-1000">
-                            <FaJsfiddle className='text-3xl text-blue-800 text-center' />
+                            {/* <FaJsfiddle className='text-3xl text-[#2F73BF] text-center' /> */}
+                            <img src='/jquery.svg' className='w-8' />
                             <span>jQuery</span>
                         </div>
                         <div className="w-36 flex flex-col gap-2 py-3 px-6 text-xs lg:text-base opacity-50 
                             lg:opacity-100 rounded-lg justify-center items-center text-center bg-[#10132E] hover:scale-125 transform duration-1000">
-                            <FaVuejs className='text-3xl text-blue-800 text-center' />
+                            <FaVuejs className='text-3xl text-[#3EB27F] text-center' />
                             <span>Vue.js</span>
                         </div>
                         <div className="w-36 flex flex-col gap-2 py-3 px-6 text-xs lg:text-base opacity-50 
                             lg:opacity-100 rounded-lg justify-center items-center text-center bg-[#10132E] hover:scale-125 transform duration-1000">
-                            <FaReact className='text-3xl text-blue-800 text-center' />
+                            <FaReact className='text-3xl text-[#47CEF6] text-center' />
                             <span>React</span>
                         </div>
                         <div className="w-36 flex flex-col gap-2 py-3 px-6 text-xs lg:text-base opacity-50 
                             lg:opacity-100 rounded-lg justify-center items-center text-center bg-[#10132E] hover:scale-125 transform duration-1000">
-                            <FaN className='text-3xl text-blue-800 text-center' />
+                            <FaN className='text-3xl text-[#F7F7F7] text-center' />
                             <span>Next.js</span>
                         </div>
                     </div>
@@ -81,56 +73,57 @@ const TechStack = () => {
         </Button>
         <Button
             duration={Math.floor(Math.random() * 10000) + 10000}
-            borderRadius="1.75rem"
-            // style={{
-            //   //   add these two
-            //   //   you can generate the color from here https://cssgradient.io/
-            //   background: "rgb(4,7,29)",
-            //   backgroundColor:
-            //     "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
-            //   // add this border radius to make it more rounded so that the moving border is more realistic
-            //   borderRadius: `calc(1.75rem* 0.96)`,
-            // }}
-            
-            className="text-white border-slate-800 bg-gradient-to-r from-slate-850 to-slate-950"
-          >
+            borderRadius="1.75rem" className="text-white border-slate-800 bg-gradient-to-r from-slate-850 to-slate-950"
+        >
                 
                 <div className='flex flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-10'>
                     <div className='text-5xl'>Backend</div>
                     <div className="flex flex-rows gap-3 md:gap-3 lg:gap-8 flex-wrap items-center justify-center">
                         <div className="w-36 flex flex-col gap-2 py-3 px-6 text-xs lg:text-base opacity-50 
                             lg:opacity-100 rounded-lg justify-center items-center text-center bg-[#10132E] hover:scale-125 transform duration-1000">
-                            <FaPhp className='text-3xl text-blue-800 text-center' />
-                            <span>PHP</span>
-                        </div>
-                        <div className="w-36 flex flex-col gap-2 py-3 px-6 text-xs lg:text-base opacity-50 
-                            lg:opacity-100 rounded-lg justify-center items-center text-center bg-[#10132E] hover:scale-125 transform duration-1000">
-                            <FaJava className='text-3xl text-blue-800 text-center' />
+                            <img src='/java.svg' className='w-8' />
                             <span>Java</span>
                         </div>
                         <div className="w-36 flex flex-col gap-2 py-3 px-6 text-xs lg:text-base opacity-50 
                             lg:opacity-100 rounded-lg justify-center items-center text-center bg-[#10132E] hover:scale-125 transform duration-1000">
-                            <FaPython className='text-3xl text-blue-800 text-center' />
+                            <FaPython className='text-3xl text-[#F6C638] text-center' />
                             <span>Python</span>
                         </div>
                         <div className="w-36 flex flex-col gap-2 py-3 px-6 text-xs lg:text-base opacity-50 
                             lg:opacity-100 rounded-lg justify-center items-center text-center bg-[#10132E] hover:scale-125 transform duration-1000">
-                            <FaLaravel className='text-3xl text-blue-800 text-center' />
-                            <span>Laravel</span>
+                            <FaPhp className='text-5xl text-[#4B588F] text-center' />
+                            <span>PHP</span>
                         </div>
                         <div className="w-36 flex flex-col gap-2 py-3 px-6 text-xs lg:text-base opacity-50 
                             lg:opacity-100 rounded-lg justify-center items-center text-center bg-[#10132E] hover:scale-125 transform duration-1000">
-                            <FaJava className='text-3xl text-blue-800 text-center' />
+                            <img src='/springboot.svg' className='w-8' />
                             <span>Spring Boot</span>
                         </div>
                         <div className="w-36 flex flex-col gap-2 py-3 px-6 text-xs lg:text-base opacity-50 
                             lg:opacity-100 rounded-lg justify-center items-center text-center bg-[#10132E] hover:scale-125 transform duration-1000">
-                            <FaDatabase className='text-3xl text-blue-800 text-center' />
+                            <FaNodeJs className='text-3xl text-[#7BB163] text-center' />
+                            <span>Node.js</span>
+                        </div>
+                        <div className="w-36 flex flex-col gap-2 py-3 px-6 text-xs lg:text-base opacity-50 
+                            lg:opacity-100 rounded-lg justify-center items-center text-center bg-[#10132E] hover:scale-125 transform duration-1000">
+                            <FaLaravel className='text-3xl text-[#F54C3A] text-center' />
+                            <span>Laravel</span>
+                        </div>
+                        <div className="w-36 flex flex-col gap-2 py-3 px-6 text-xs lg:text-base opacity-50 
+                            lg:opacity-100 rounded-lg justify-center items-center text-center bg-[#10132E] hover:scale-125 transform duration-1000">
+                            <FaFire className='text-3xl text-[#F54C3A] text-center' />
+                            <span>CodeIgniter</span>
+                        </div>
+                        <div className="w-36 flex flex-col gap-2 py-3 px-6 text-xs lg:text-base opacity-50 
+                            lg:opacity-100 rounded-lg justify-center items-center text-center bg-[#10132E] hover:scale-125 transform duration-1000">
+                            {/* <FaDatabase className='text-3xl text-[#3F779C] text-center' /> */}
+                            <img src='/postgresql.svg' className='w-8' />
                             <span>PostgreSQL</span>
                         </div>
                         <div className="w-36 flex flex-col gap-2 py-3 px-6 text-xs lg:text-base opacity-50 
                             lg:opacity-100 rounded-lg justify-center items-center text-center bg-[#10132E] hover:scale-125 transform duration-1000">
-                            <FaDatabase className='text-3xl text-blue-800 text-center' />
+                            {/* <FaDatabase className='text-3xl text-[#035D85] text-center' /> */}
+                            <img src='/mysql.svg' className='w-8' />
                             <span>MySQL</span>
                         </div>
                     </div>
