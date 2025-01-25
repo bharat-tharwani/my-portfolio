145d37c60e36a87b0d8817aa4a45c1da0f71fe4f
Bharat <btharwani92@gmail.com>
2025-01-25 18:26:57 -0500

    Fixed image optimization issue

--- components/ui/infinite-cards.tsx
+++ components/ui/infinite-cards.tsx
@@ -88,110 +88,124 @@ export const InfiniteMovingCards = ({
           pauseOnHover && "hover:[animation-play-state:paused]"
         )}
       >
-  
-          <li className="w-[30vw] relative rounded-2xl border border-b-0
+        <li
+          className="w-[30vw] relative rounded-2xl border border-b-0
              flex-shrink-0 border-slate-800 p-5"
-            style={{
+          style={{
+            background: "rgb(4,7,29)",
+            backgroundColor:
+              "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+          }}
+        >
+          <div>
+            <h1 className="text-xl text-purple-400 text-center tracking-widest mb-5">
+              Cooking
+            </h1>
+            <Image
+              src="/cooking.gif"
+              alt="startup template"
+              width={500}
+              height={500}
+              unoptimized
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+          </div>
+        </li>
 
-              background: "rgb(4,7,29)",
-              backgroundColor:
-                "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
-            }}
-          >
-            <div>
-              <h1 className="text-xl text-purple-400 text-center tracking-widest mb-5">Cooking</h1>
-              <Image
-                src="/cooking.gif"
-                alt="startup template"
-                width={500}
-                height={500}
-                className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-              />
-            </div>
-          </li>
-
-          <li className="w-[30vw] relative rounded-2xl border border-b-0
+        <li
+          className="w-[30vw] relative rounded-2xl border border-b-0
              flex-shrink-0 border-slate-800 p-5"
-            style={{
+          style={{
+            background: "rgb(4,7,29)",
+            backgroundColor:
+              "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+          }}
+        >
+          <div>
+            <h1 className="text-xl text-purple-400 text-center tracking-widest mb-5">
+              Ping Pong
+            </h1>
+            <Image
+              src="/ping-pong.gif"
+              alt="startup template"
+              width={500}
+              height={500}
+              unoptimized
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+          </div>
+        </li>
 
-              background: "rgb(4,7,29)",
-              backgroundColor:
-                "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
-            }}
-          >
-            <div>
-              <h1 className="text-xl text-purple-400 text-center tracking-widest mb-5">Ping Pong</h1>
-              <Image
-                src="/ping-pong.gif"
-                alt="startup template"
-                width={500}
-                height={500}
-                className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-              />
-            </div>
-          </li>
-            
-          <li className="w-[30vw] relative rounded-2xl border border-b-0
+        <li
+          className="w-[30vw] relative rounded-2xl border border-b-0
              flex-shrink-0 border-slate-800 p-5"
-            style={{
-
-              background: "rgb(4,7,29)",
-              backgroundColor:
-                "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
-            }}
-          >
-            <div>
-              <h1 className="text-xl text-purple-400 text-center tracking-widest mb-5">Cricket</h1>
-              <Image
-                src="/cricket.gif"
-                alt="startup template"
-                width={500}
-                height={500}
-                className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-              />
-            </div>
-          </li>
-          <li className="w-[30vw] relative rounded-2xl border border-b-0
+          style={{
+            background: "rgb(4,7,29)",
+            backgroundColor:
+              "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+          }}
+        >
+          <div>
+            <h1 className="text-xl text-purple-400 text-center tracking-widest mb-5">
+              Cricket
+            </h1>
+            <Image
+              src="/cricket.gif"
+              alt="startup template"
+              width={500}
+              height={500}
+              unoptimized
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+          </div>
+        </li>
+        <li
+          className="w-[30vw] relative rounded-2xl border border-b-0
              flex-shrink-0 border-slate-800 p-5"
-            style={{
+          style={{
+            background: "rgb(4,7,29)",
+            backgroundColor:
+              "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+          }}
+        >
+          <div>
+            <h1 className="text-xl text-purple-400 text-center tracking-widest mb-5">
+              Chess
+            </h1>
+            <Image
+              src="/chess_animated.gif"
+              alt="startup template"
+              width={500}
+              height={500}
+              unoptimized
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+          </div>
+        </li>
 
-              background: "rgb(4,7,29)",
-              backgroundColor:
-                "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
-            }}
-          >
-            <div>
-              <h1 className="text-xl text-purple-400 text-center tracking-widest mb-5">Chess</h1>
-              <Image
-                src="/chess_animated.gif"
-                alt="startup template"
-                width={500}
-                height={500}
-                className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-              />
-            </div>
-          </li>    
-
-          <li className="w-[30vw] relative rounded-2xl border border-b-0
+        <li
+          className="w-[30vw] relative rounded-2xl border border-b-0
              flex-shrink-0 border-slate-800 p-5"
-            style={{
-
-              background: "rgb(4,7,29)",
-              backgroundColor:
-                "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
-            }}
-          >
-            <div>
-              <h1 className="text-xl text-purple-400 text-center tracking-widest mb-5">TV</h1>
-              <Image
-                src="/tv.gif"
-                alt="startup template"
-                width={500}
-                height={500}
-                className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-              />
-            </div>
-          </li>          
+          style={{
+            background: "rgb(4,7,29)",
+            backgroundColor:
+              "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+          }}
+        >
+          <div>
+            <h1 className="text-xl text-purple-400 text-center tracking-widest mb-5">
+              TV
+            </h1>
+            <Image
+              src="/tv.gif"
+              alt="startup template"
+              width={500}
+              height={500}
+              unoptimized
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+          </div>
+        </li>
       </ul>
     </div>
   );
